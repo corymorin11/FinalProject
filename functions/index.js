1f0b25402bfd8e9a532a808b9ed12ec32b988aff
corymorin11 <54879258+corymorin11@users.noreply.github.com>
2019-12-07 13:13:39 -0700

    Finished Final Project

--- functions/index.js
+++ functions/index.js
@@ -21,17 +21,22 @@ const createProfile = (userRecord, context) => {
     .catch(console.error);
 };
 
-const AddRequest = (data, number) => {
-  db.collection().add({
-    number: number,
-    data: data
-  });
+const AddRequest = (message, from, list) => {
+  if (list === "1") {
+    db.collection("grocery-orders").add({ message: message, from: from });
+  } else if (list === "2") {
+    db.collection("office-orders").add({ message: message, from: from });
+  } else if (list === "3") {
+    db.collection("grocery-photos").add({ message: message, from: from });
+  } else if (list === "4") {
+    db.collection("office-photos").add({ message: message, from: from });
+  }
 };
 
 module.exports = {
   authOnCreate: functions.auth.user().onCreate(createProfile),
   twiliohook: functions.https.onRequest((req, res) => {
     console.log(req.body.From, req.body.Body);
-    AddRequest(req.body.Body, req.body.From);
+    AddRequest(req.body.message, req.body.from, req.body.list);
   })
 };
